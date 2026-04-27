52ab3e998d483e7534baef2918b22ff3ef093dd5
sreea05 <258596614+sreea05@users.noreply.github.com>
2026-04-27 19:02:45 +0100

    C++: Add sample matrix function

--- cpp/lib/src/quant_finance.cpp
+++ cpp/lib/src/quant_finance.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+
 #include "quant_finance/quant_finance.hpp"
 
 namespace quant_finance
 {
+    Eigen::MatrixXd scale_matrix(const double scale)
+    {
+        Eigen::MatrixXd mat(3, 3);
+        // clang-format off
+        mat <<  1, 2, 3,
+                4, 5, 6,
+                7, 8, 9;
+        // clang-format on
+
+        std::cout << mat.data() << std::endl;
+
+        mat *= scale;
+        return mat;
+    }
+
     double add(double a, double b)
     {
         return a + b;
