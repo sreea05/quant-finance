57cc4069aa8e15b17f345340ffc5a55db4049be9
sreea05 <258596614+sreea05@users.noreply.github.com>
2026-05-05 20:47:14 +0100

    C++: Update documentation

---
+++ cpp/lib/inc/quant_finance/groups.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file groups.hpp
+ * @brief Doxygen group definitions for the quant_finance library.
+ *
+ * This file exists solely to declare Doxygen groups. It is never included
+ * by C++ translation units. To add a symbol to a group, annotate it with
+ * \@ingroup group_id in its own header.
+ *
+ * To add a new group, add a \@defgroup block below.
+ */
+
+/**
+ * @defgroup qf_classes Classes
+ * @brief All public classes in the quant_finance library.
+ */
+
+/**
+ * @defgroup qf_functions Free Functions
+ * @brief Standalone free functions in the quant_finance library.
+ */
+
+/**
+ * @defgroup qf_types Type Aliases
+ * @brief Common type aliases used throughout the quant_finance library.
+ */
--- cpp/lib/inc/quant_finance/math/types.hpp
+++ cpp/lib/inc/quant_finance/math/types.hpp
@@ -10,9 +10,11 @@ namespace qf
     namespace math
     {
         /// @brief A dynamic-size column vector of double-precision floating-point values.
+        /// @ingroup qf_types
         using vector = Eigen::VectorXd;
 
         /// @brief A dynamic-size matrix of double-precision floating-point values.
+        /// @ingroup qf_types
         using matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
     } // namespace math
 } // namespace qf
